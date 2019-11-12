ead7bdec5cccd253e7182ae744d7a953ef24507c
lilach199612 <lilach199612@gmail.com>
2019-11-12 13:29:50 +0200

    power.c

--- power.c
+++ power.c
@@ -4,10 +4,15 @@
 double Pow(double x,int y){
 	int i;
 	double pow=1;
+	int minus=0;
+	if(y<0)
+		minus=-1;
 	for(i=1;i<=y;i=i+1)
 	{
 		pow=pow*x;
 	}
+	if(minus==-1)
+		pow=1/pow;
 	return pow;
 }
 
