a53c4d8b6edf07a92f141e792681a013e658eefc
lilach199612 <lilach199612@gmail.com>
2019-11-12 14:02:09 +0200

    power.c

--- power.c
+++ power.c
@@ -2,12 +2,13 @@
 #define e 2.71828182846
 
 double Pow(double x,int y){
-	int i;
 	double pow=1;
 	int minus=0;
-	if(y<0)
+	if(y<0){
 		minus=-1;
-	for(i=1;i<=y;i=i+1)
+		y=y*(-1);
+	}
+	for(int i=1;i<=y;i=i+1)
 	{
 		pow=pow*x;
 	}
